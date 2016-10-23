2a6f2623925021957b13e9fd58f5e2d9c74fbbea
Steffen Bleul <sbl@itbleul.de>
2016-10-23 10:53:50 +0200

    New Alpine version

--- build/settings.groovy
+++ build/settings.groovy
@@ -6,7 +6,7 @@
  * Build parameters, must be adjusted when forked!
  **/
 dockerImageName = 'blacklabelops/alpine'
-dockerTags = ["latest","3.3"] as String[]
+dockerTags = ["latest","3.4"] as String[]
 dockerTestCommands =
  ["echo hello world",
   "ps -All",
@@ -15,7 +15,7 @@ dockerTestCommands =
   "cat /etc/hosts",
   "cat /etc/passwd"] as String[]
 dockerRepositories = [["","Dockerhub","DockerhubEmail"]] as String[][]
-dockerImages = ["alpine:3.3"] as String[]
+dockerImages = ["alpine:3.4"] as String[]
 
 def getBranchName() {
   def branchName = env.JOB_NAME.replaceFirst('.+/', '')
